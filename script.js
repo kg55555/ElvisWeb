0f90860ee19575b6b5e16771d41ac99fac1d674f
Kevin <kg25000@gmail.com>
2023-06-27 23:01:07 -0700

    Horizontal Scrollilng, Portfolio Hover, Menu transition

--- script.js
+++ script.js
@@ -5,26 +5,47 @@ let scaleFactor = 1.5;
 let scrollY = 0;
 let carouselScroll = 0;
 
+let projects = ["pondr", "hitachi", "chexy", "rate", "uxcol", "neri", "troop", "sfu", "dare"];
+
+projects.forEach(element => {
+    let projectOne = document.getElementById(element);
+    let projectOneText = document.getElementById(element + "-text");
+    let projectOneDesc = document.getElementById(element + "-desc");
+
+    projectOne.addEventListener('mouseover', (event) => {
+        projectOneText.style.opacity = 0;
+        projectOneDesc.style.opacity = 1;
+
+})
+    projectOne.addEventListener('mouseout', (event) => {
+        projectOneText.style.opacity = 1;
+        projectOneDesc.style.opacity = 0;
+
+    })
+});
+
+
 document.addEventListener('scroll', (event) => {
     setTimeout(function() {
     let winHeight = window.innerHeight;
     let about = document.getElementById("about");
     let portfolio = document.getElementById("portfolio");
     let pictures = document.getElementById("pictures");
+
+    let pageHeader = document.getElementById("page-header");
+
     let picturesCarousel = document.getElementById("pictures-carousel");
     scrollY = window.scrollY;
 
-    if (scrollY >= scaleFactor * 3 * winHeight) {
-        event.preventDefault();
-
-        carouselScroll = scrollY - scaleFactor * 3 * winHeight;
-        picturesCarousel.scrollLeft += carouselScroll * 0.2;
-        return;
-    }
-
-
     about.style.setProperty("top", scrollY * 0.8 + "px");
 
+    if (scrollY > 2 * winHeight) {
+        pageHeader.style.backgroundColor = "transparent";
+        pageHeader.style.color = "white";
+    } else {
+        pageHeader.style.backgroundColor = "#c1c1c1";
+        pageHeader.style.color = "black";
+    }
 
     if (scrollY > 0 && scrollY <= 2 * 2 * winHeight) {
         portfolioFinalPos = scaleFactor * winHeight + window.scrollY / 3 ;
@@ -38,8 +59,20 @@ document.addEventListener('scroll', (event) => {
         pictures.style.top = picturesTransitionPos + scaleFactor * winHeight + (scrollY - portfolioScroll) / 2 + "px"; 
     }
 
+    if (scrollY > 4.52 * winHeight && scrollY < 7.67 * winHeight) {
+        picturesCarousel.style.position = "fixed";
+        picturesCarousel.style.marginTop = "10vh";
+        picturesCarousel.style.top = scrollY * 0.001 + 'px';
+        picturesCarousel.style.left = -(scrollY - 4.52 * winHeight) * 0.55 + 'px';
+    } else if (scrollY <= 4.52 * winHeight) {
+        picturesCarousel.style.position = "initial";
+        picturesCarousel.style.marginTop = "0";
+    } else {
+        picturesCarousel.style.position = "fixed";
+        picturesCarousel.style.marginTop = (winHeight / 10 - (scrollY - 7.67 * winHeight)) + "px";
+        picturesCarousel.style.top = scrollY * 0.001 + 'px';
+    }
 
-    console.log(scrollY);
     }, 0);
 
 })
